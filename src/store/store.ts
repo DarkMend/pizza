d87c7b9cc9e6ea0a314b660a6598001371531f1e
DarkMend <gindullin20@mail.ru>
2024-10-09 01:01:29 +0300

    fff

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "./user.slice";
+import userSlice from "./user.slice";
 
 export const store = configureStore({
     reducer: {
-        
+        user: userSlice
     }
 })
 
